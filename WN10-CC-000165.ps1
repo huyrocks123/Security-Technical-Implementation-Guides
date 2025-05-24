972b9131f8142910f88a60aeb6549bbd66ad6417
Huy Tang <40283255+huyrocks123@users.noreply.github.com>
2025-05-24 14:15:49 -0400

    Create WN10-CC-000165.ps1

---
+++ WN10-CC-000165.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+This PowerShell script ensures the 'RestrictRemoteClients' registry key is set to 1 under 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Rpc'
+
+.NOTES
+    Author          : Huy Tang
+    LinkedIn        : linkedin.com/in/huy-t-892a51317/
+    GitHub          : github.com/huyrocks123
+    Date Created    : 2025-05-24
+    Last Modified   : 2025-05-24
+    Version         : 1.0
+    STIG-ID         : WN10-CC-000165
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run as Administrator  
+    PS C:\> .\WN10-CC-000165.ps1
+#>
+
+# Define registry path and values
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Rpc"
+$valueName = "RestrictRemoteClients"
+$desiredValue = 1
+
+# Ensure the registry path exists
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the required registry value
+Set-ItemProperty -Path $regPath -Name $valueName -Value $desiredValue -Type DWord
+
+# Output success message
+Write-Host "'$valueName' has been successfully set to '$desiredValue' as required."
